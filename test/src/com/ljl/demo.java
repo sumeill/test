8e9d3d940d52c7e1f248de2d34f8018c4b2e6283
HEMO-1 <1361708796@qq.com>
2020-06-18 17:08:20 +0800

    liang9

--- test/src/com/ljl/demo.java
+++ test/src/com/ljl/demo.java
@@ -3,11 +3,7 @@ package com.ljl;
 public class demo {
 
     public static void main(String[] args) {
-        String str = "18  4 567       99     56";
-        String[] strArray = str.split(" +");
-        for (int i = 0; i < strArray.length; i++) {
-            System.out.println(strArray[i]);
-        }
+        System.out.println("1");
     }
 
 }
