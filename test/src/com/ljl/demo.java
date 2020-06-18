c635e05b2e6152c1fea823606aaa93ebe918383b
HEMO-1 <1361708796@qq.com>
2020-06-18 16:05:40 +0800

    liang1

---
+++ test/src/com/ljl/demo.java
@@ -0,0 +1,13 @@
+package com.ljl;
+
+public class demo {
+
+    public static void main(String[] args) {
+        String str = "18  4 567       99     56";
+        String[] strArray = str.split(" +");
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+    }
+
+}
